3b4f49e7ea89a44438355e74e4866efbb270c45a
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-23 12:12:27 -0300

    feature: implementando atributos da classe Pessoa

---
+++ backend/oscar/src/main/java/com/oscar/model/Pesoa.java
@@ -0,0 +1,39 @@
+package com.oscar.model;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotNull;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Pesoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PESSOA")
+	private Integer id;
+
+	@Column(name = "NOME_ARTISTICO", length = 64)
+	@NotNull
+	private String nomeArtistico;
+
+	@Column(name = "NOME_VERDADEIRO", length = 64)
+	@NotNull
+	private String nomeVerdadeiro;
+
+	@Column(name = "SEXO")
+	private Sexo sexo;
+
+	@Column(name = "DATA_NASCIMENTO")
+	private LocalDate Nascimento;
+
+	@Column(name = "SITE")
+	private String site;
+
+	@Column(name = "ANO_INICIO_CARREIRA")
+	private Integer anoInicioCarreira;
+
+}
---
+++ backend/oscar/src/main/java/com/oscar/model/Sexo.java
@@ -0,0 +1,7 @@
+package com.oscar.model;
+
+public enum Sexo {
+	MASCULINO,
+	FEMININO,
+	OUTRO
+}
