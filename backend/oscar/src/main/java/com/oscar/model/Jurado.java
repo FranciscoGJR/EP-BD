e72002d4d0f4543d1a58951caf191c26ac5bc870
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-23 14:04:19 -0300

    feature: implementando atributos da classe Jurado

---
+++ backend/oscar/src/main/java/com/oscar/model/Jurado.java
@@ -0,0 +1,15 @@
+package com.oscar.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Jurado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_JURADO")
+	Integer idJurado;
+
+}
