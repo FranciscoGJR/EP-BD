2eb89e3ffef2517ce2dff1af7e502eeb48a46484
FranciscoGJR <franciscoogjr@gmail.com>
2023-12-31 20:30:35 -0300

    feature: implementando insercao de Pessoa no BD

--- backend/oscar/src/main/java/com/oscar/repository/PessoaRepository.java
+++ backend/oscar/src/main/java/com/oscar/repository/PessoaRepository.java
@@ -1,18 +1,11 @@
 package com.oscar.repository;
 
-import java.time.LocalDate;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.oscar.model.Pessoa;
-import com.oscar.model.Sexo;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-	
-	@Query(value = "INSERT INTO PESSOA p (id, nomeArtistico, nomeVerdadeiro, sexo, Nascimento, site, anoInicioCarreira) VALUES(:id, :nomeArtistico, :nomeVerdadeiro, :sexo, :nascimento, :site, :anoInicioCarreira)", nativeQuery = true)
-	void save(Integer id, String nomeArtistico, String nomeVerdadeiro, Sexo sexo, LocalDate nascimento, String site , Integer anoInicioCarreira);
-	
+
 }
--- backend/oscar/src/main/java/com/oscar/service/PessoaService.java
+++ backend/oscar/src/main/java/com/oscar/service/PessoaService.java
@@ -4,16 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.oscar.model.Pessoa;
-import com.oscar.repository.PessoaRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 @Service
 public class PessoaService {
 
 	@Autowired
-	private PessoaRepository pessoaRepository;
+	private EntityManager entityManager;
 
+	@Transactional
 	public void save(Pessoa pessoa) {
-		pessoaRepository.save(pessoa.getId(), pessoa.getNomeArtistico(), pessoa.getNomeVerdadeiro(), pessoa.getSexo(), pessoa.getNascimento(), pessoa.getSite(), pessoa.getAnoInicioCarreira());
+		entityManager.createNativeQuery(
+				"INSERT INTO PESSOA (ID_PESSOA, NOME_ARTISTICO, NOME_VERDADEIRO, SEXO, DATA_NASCIMENTO, SITE, ANO_INICIO_CARREIRA) VALUES(? , ? , ? , ? , ? , ? , ?)")
+				.setParameter(1, pessoa.getId()).setParameter(2, pessoa.getNomeArtistico())
+				.setParameter(3, pessoa.getNomeVerdadeiro()).setParameter(4, pessoa.getSexo())
+				.setParameter(5, pessoa.getNascimento()).setParameter(6, pessoa.getSite())
+				.setParameter(7, pessoa.getAnoInicioCarreira()).executeUpdate();
 	}
 
 }
